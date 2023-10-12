aac52bf5fb19b9e902c8b813be98e5892ce53458
Davide Lombardi <davidelombardi130p@gmail.com>
2023-10-12 18:11:08 +0200

    added godotenv for env vars

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	app := fiber.New()
+  // Reading env vars
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+  port := "3000"
+  if os.Getenv("PORT") != "" {
+    port = os.Getenv("PORT")
+  }
+  isProd := false
+  if os.Getenv("ENVIRONMENT") == "PRODUCTION" {
+    isProd = true
+  }
+
+	app := fiber.New(fiber.Config{
+    Prefork: isProd,
+  })
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Pulpmovies - Backend API Service")
 	})
 
-	app.Listen(":3001")
+	app.Listen(":" + port)
 
 }
