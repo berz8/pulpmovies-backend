bc5fd1ff35d477ae867bf613406ebf29b5afaa99
Davide Lombardi <davidelombardi130p@gmail.com>
2023-10-14 11:16:35 +0200

    users table constraints

--- database/migrations/users.go
+++ database/migrations/users.go
@@ -5,15 +5,15 @@ import "database/sql"
 func CreateUserTable(db *sql.DB) error {
 	_, err := db.Exec(`
     CREATE TABLE IF NOT EXISTS users (
-      id INTEGER,
-      username TEXT,
-      email TEXT,
+      id INTEGER NOT NULL UNIQUE,
+      username TEXT UNIQUE,
+      email TEXT UNIQUE,
       full_name TEXT,
       birthday TEXT,
       biography TEXT,
       profile_path TEXT,
-      account_status TEXT,
-      onboarding INTEGER,     
+      account_status TEXT NOT NULL,
+      onboarding INTEGER NOT NULL,     
       created_at TEXT,
       updated_at TEXT,
       deleted_at TEXT,
